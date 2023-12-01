ea51bb1a02b7e52c1014b5db86614245aa1983ca
Mariam Tamrazyan <mart76901@gmail.com>
2023-12-01 12:02:10 +0400

    The project is configured to run

--- config/settings.py
+++ config/settings.py
@@ -85,18 +85,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medcenter',
-        'USER': 'postgres',
-        'PASSWORD': 1111,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -122,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Asia/Yerevan'
+TIME_ZONE = os.getenv("TIME_ZONE")
 
 USE_I18N = True
 
